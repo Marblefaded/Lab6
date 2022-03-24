c98c944aeded86df4fd8ef1a7057567e97c1fbb2
Marblefade <sitnikov.vladimiir@mail.ru>
2022-03-24 16:53:52 +0700

    Добавьте файлы проекта.

---
+++ App.axaml.cs
@@ -0,0 +1,31 @@
+using lab6.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using lab6.ViewModels;
+using lab6.Views;
+
+
+namespace Lab6
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
